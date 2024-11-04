c0c513d63255cb4751fa5afc6d1f070753f98222
vachg <vachgen@yandex.ru>
2024-11-04 15:58:49 +0300

    educationSpring

---
+++ src/main/java/pro/sky/proskyhellodemo/ProSkyHelloDemoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.proskyhellodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProSkyHelloDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProSkyHelloDemoApplication.class, args);
+	}
+
+}
